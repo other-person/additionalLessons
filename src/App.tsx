a7b43f54c04e8225879caf293c5b49571f58e0ef
Tarikclear <bazhenkataras@gmail.com>
2022-04-15 17:09:08 +0400

    Add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Header} from "./Site/Header";
 import {Footer} from "./Site/Footer";
@@ -9,42 +9,59 @@ import {Button} from "./NewComponent/Button";
 
 function App() {
 
-    const students = [
-        {id: 1, name: "Tarik", age: 34},
-        {id: 2, name: "Nik", age: 24},
-        {id: 3, name: "Ivan", age: 44},
-        {id: 4, name: "Vova", age: 55},
-        {id: 5, name: "Serg", age: 23},
-    ]
+    // const students = [
+    //     {id: 1, name: "Tarik", age: 34},
+    //     {id: 2, name: "Nik", age: 24},
+    //     {id: 3, name: "Ivan", age: 44},
+    //     {id: 4, name: "Vova", age: 55},
+    //     {id: 5, name: "Serg", age: 23},
+    // ]
+    //
+    // const topCars = [
+    //     {manufacturer: 'Mercedes', model: "a1v3 "},
+    //     {manufacturer: 'Audi', model: "a6"},
+    //     {manufacturer: 'Jaguar', model: "s2"},
+    //     {manufacturer: 'BMW', model: "e39"},
+    // ]
+    // const Button1Foo = (name: string, age: number) => {
+    //     console.log(name, age)
+    // }
+    // const Button2Foo = (name: string, age: number) => {
+    //     console.log(name, age)
+    // }
+    // const Button3Foo = () => {
+    //     console.log("Im stupid button")
+    // }
 
-    const topCars = [
-        {manufacturer: 'Mercedes', model: "a1v3 "},
-        {manufacturer: 'Audi', model: "a6"},
-        {manufacturer: 'Jaguar', model: "s2"},
-        {manufacturer: 'BMW', model: "e39"},
-    ]
-    const Button1Foo = (name: string , age: number) => {
-        console.log(name, age)
-    }
-    const Button2Foo = (name: string , age: number) => {
-        console.log(name, age)
-    }
-    const Button3Foo = () => {
-        console.log("Im stupid button")
-    }
 
+    let [a, setA] = useState(0)
 
+    const onClickHandler = () => {
+        setA(++a);
+        //console.log(a)
+    }
+    const onClickHandlerZero = () => {
+        setA(0)
+    }
     return (
         <div className="App">
 
-            {/*действия button*/}
+            {/*useState*/}
             <div>
-
-                <Button name={"MyYouTubeChannel_1"} callBack={()=>Button1Foo("Ivan",21)}/>
-                <Button name={"MyYouTubeChannel_2"} callBack={()=>Button2Foo("Vasia",44)}/>
-                <Button name={"MyYouTubeChannel_3"} callBack={Button3Foo}/>
-
+                <h1>{a}</h1>
             </div>
+            <button onClick={onClickHandler}>Add +1</button>
+            <button onClick={onClickHandlerZero}>Zero</button>
+
+
+            {/*/!*действия button*!/*/}
+            {/*<div>*/}
+
+            {/*    <Button name={"MyYouTubeChannel_1"} callBack={()=>Button1Foo("Ivan",21)}/>*/}
+            {/*    <Button name={"MyYouTubeChannel_2"} callBack={()=>Button2Foo("Vasia",44)}/>*/}
+            {/*    <Button name={"MyYouTubeChannel_3"} callBack={Button3Foo}/>*/}
+
+            {/*</div>*/}
 
             {/*Вложенная компонента*/}
             {/*<div>*/}
@@ -59,9 +76,11 @@ function App() {
             {/*    <Cars cars = {topCars}/>*/}
             {/*</div>*/}
 
+            {/*UseState*/}
+
 
         </div>
-);
+    );
 }
 
 export default App;
